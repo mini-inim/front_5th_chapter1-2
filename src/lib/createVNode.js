c322305716bdceefbf699021a7d8f6c2189967c7
mini--inim <forestaroundseoul@gmail.com>
2025-03-31 21:52:16 +0900

    createVNode 함수 작성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,9 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children
+      .flat(Infinity)
+      .filter((child) => child != null && child !== false),
+  };
 }
