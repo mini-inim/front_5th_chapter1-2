207e8aa77b826a0061e5b42640e4e431862f2db4
mini--inim <forestaroundseoul@gmail.com>
2025-04-01 01:34:11 +0900

    feat: normalizeVNode 생성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,23 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode === "boolean" || vNode === null || vNode === undefined) {
+    return "";
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props || null,
+    children: vNode.children
+      .flatMap(normalizeVNode)
+      .filter((node) => node !== ""),
+  };
 }
