04a131ff5bb06cb72caaa8e16b77e591cfbab280
mini--inim <forestaroundseoul@gmail.com>
2025-04-02 21:47:50 +0900

    feat: eventManager 기능

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,35 @@
-export function setupEventListeners(root) {}
+const eventStore = new Map(); //이벤트 저장
+const eventTypes = new Set(); //이벤트 타입 저장
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventTypes.forEach((eventType) => {
+    root.addEventListener(eventType, eventHandler);
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+//이벤트 헨들러
+function eventHandler(e) {
+  const target = e.target;
+
+  const handlerEvent = eventStore.get(target)?.get(e.type);
+  handlerEvent?.(e);
+}
+
+export function addEvent(element, eventType, handler) {
+  eventTypes.add(eventType);
+
+  if (!eventStore.has(element)) {
+    eventStore.set(element, new Map());
+  }
+
+  const elEvent = eventStore.get(element);
+  elEvent.set(eventType, handler);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const isHandler = eventStore.get(element);
+
+  if (isHandler.has(eventType)) {
+    isHandler.delete(eventType);
+  }
+}
