142832dd5d3f422ac710b9d7044cfbba16fc28ca
mini--inim <forestaroundseoul@gmail.com>
2025-04-04 04:17:16 +0900

    feat: 포스트 가드 추가

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,6 +1,7 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
 import { toTimeFormat } from "../../utils/index.js";
+import { globalStore } from "../../stores";
 
 export const Post = ({
   author,
@@ -9,6 +10,25 @@ export const Post = ({
   likeUsers,
   activationLike = false,
 }) => {
+  const { loggedIn, currentUser, posts } = globalStore.getState();
+  const isLike = currentUser && likeUsers.includes(currentUser.username);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (!loggedIn) {
+      return alert("로그인 후 이용해주세요");
+    }
+
+    // const post = posts.find((post) => post.id === id);
+
+    // if(post.likeUsers.includes(currentUser.username)){
+
+    // }
+
+    globalStore.setState({ posts });
+  };
+
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -21,6 +41,7 @@ export const Post = ({
       <div className="mt-2 flex justify-between text-gray-500">
         <span
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          onClick={handleClick}
         >
           좋아요 {likeUsers.length}
         </span>
